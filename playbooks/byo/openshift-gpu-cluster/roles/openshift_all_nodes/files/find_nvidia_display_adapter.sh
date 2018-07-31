591499da3d592c2898fe9dc5efe0fc1d152941b1
Zvonko Kosic <zvonkok@gmail.com>
2018-07-31 11:07:10 +0200

    Added some tweask

--- playbooks/byo/openshift-gpu-cluster/roles/openshift_all_nodes/files/find_nvidia_display_adapter.sh
+++ playbooks/byo/openshift-gpu-cluster/roles/openshift_all_nodes/files/find_nvidia_display_adapter.sh
@@ -13,21 +13,19 @@ do
     CLASS=`cat $i/class`
 
     if [ $(( ${VENDOR} ^ ${VENDOR_ID} )) == 0 ]; then
-        echo "Found NVIDIA PCI card"
+        # echo "Found NVIDIA PCI card"
         # 0x300=VGA 0x302=3D
         CLASS_DISPLAY=$(( ${CLASS} & 0xff0000 ))
         if [ $(( ${CLASS_DISPLAY} ^ 0x030000 )) == 0 ]; then
-            echo "Found NVIDIA PCI display adapter"
+            #echo "Found NVIDIA PCI display adapter"
+            echo 0
             exit 0
         fi
 
     fi
 done
 
-exit 1
+echo 1
 
 
-#crw-rw-rw-. 1 root root 195,   0 Nov 20 10:20 /dev/nvidia0
-#crw-rw-rw-. 1 root root 195, 255 Nov 20 10:20 /dev/nvidiactl
-#crw-rw-rw-. 1 root root 245,   0 Nov 20 10:50 /dev/nvidia-uvm
-#crw-rw-rw-. 1 root root 245,   1 Nov 20 10:50 /dev/nvidia-uvm-tools
+
